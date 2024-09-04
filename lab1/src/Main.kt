329188ec740b14652b415bcef7c9a038ffb1d76f
Murlocki <kirillkornilov03@gmail.com>
2024-09-05 00:24:26 +0300

    Lab 1.3. Убран повторяющийся код в конструкторе,геттере и сеттерах за счет переделки класса в data class

--- lab1/src/Main.kt
+++ lab1/src/Main.kt
@@ -3,9 +3,9 @@
 fun main() {
     val students = mutableListOf<Student>();
     students.add(Student("aaaaa","bbbbbb","cccccc"));
-    students.add(Student("34","bbbbbb","cccccc", studentTelegram = "@elelelelele"));
-    students.add(Student("444","bbbbbb","cccccc", studentGit = "eeefefefef"));
-    students.add(Student("AB","bbbbbb","cccccc", studentEmail = "kk@gmial.com"));
+    students.add(Student("34","bbbbbb","cccccc", telegram = "@elelelelele"));
+    students.add(Student("444","bbbbbb","cccccc", gitHub = "eeefefefef"));
+    students.add(Student("AB","bbbbbb","cccccc", email = "kk@gmial.com"));
     students.add(Student("B","bbbbbb","cccccc"));
     students.add(Student("C","bbbbbb","cccccc"));
     students.add(Student("D","bbbbbb","cccccc"));
