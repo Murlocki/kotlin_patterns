a7f35430f46ce0e3d243949b81e048db84b58196
Murlocki <kirillkornilov03@gmail.com>
2024-09-06 01:32:53 +0300

    Lab 1.10 Добавлена схема класса

--- lab1/src/Student.kt
+++ lab1/src/Student.kt
@@ -96,42 +96,42 @@ data class Student(private var surname:String,
 
     companion object{
         // Автосоздание id
-        var classId:Int = 0
-        fun autoGenerateId():Int{
+        private var classId:Int = 0
+        private fun autoGenerateId():Int{
             classId+=1
             return classId
         }
 
         // Валидация телефона
-        fun isValidPhone(phone: String?): Boolean {
+        private fun isValidPhone(phone: String?): Boolean {
             return phone?.matches(Regex("\\+7\\d{10}")) ?: true
         }
 
         //Валидация фамилии
-        fun isValidSurname(surname: String): Boolean {
+        private  fun isValidSurname(surname: String): Boolean {
             return surname.matches(Regex("^[A-Z][a-z]*(-([A-Za-z]?)[a-z]*)*$"))
         }
         //Валидация имени
-        fun isValidName(name: String): Boolean {
+        private  fun isValidName(name: String): Boolean {
             return name.matches(Regex("^[A-Z][a-z]*(-([A-Za-z]?)[a-z]*)*$"))
         }
         //Валидация отчества
-        fun isValidPatronymic(patronymic: String): Boolean {
+        private  fun isValidPatronymic(patronymic: String): Boolean {
             return patronymic.matches(Regex("^[A-Z][a-z]*(-([A-Za-z]?)[a-z]*)*$"))
         }
 
         //Валидация телеграм
-        fun isValidTelegram(telegram: String?): Boolean {
+        private  fun isValidTelegram(telegram: String?): Boolean {
             return telegram?.matches(Regex("@(?=.{5,64})(?!_)(?!.*__)[a-zA-Z0-9_]+(?<![_.])")) ?: true
         }
 
         //Валидация почты
-        fun isValidEmail(email: String?): Boolean {
+        private  fun isValidEmail(email: String?): Boolean {
             return email?.matches(Regex("^[a-zA-Z][a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$")) ?: true
         }
 
         //Валидация гита
-        fun isValidGitHub(gitHub: String?): Boolean {
+        private  fun isValidGitHub(gitHub: String?): Boolean {
             return gitHub?.let { !Regex("[$%#@&/?]").matches(it) } ?: true
         }
 
