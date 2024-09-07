2803044531fed236d136d26a3371325c8c849397
Murlocki <kirillkornilov03@gmail.com>
2024-09-07 16:34:26 +0300

    Lab 1 Рефакторинг кода

--- lab1/src/Main.kt
+++ lab1/src/Main.kt
@@ -3,17 +3,19 @@
 fun main() {
     val students = mutableListOf<Student>();
     students.add(Student("Aaaaa","Bbbbbb","Cccccc"));
-    students.add(Student("Aabbb-Abbb","Bbbbbb","Cccccc", telegram = "@elelelelele"));
-    students.add(Student("Aaabbb","Bbb-Cbbb","Cccccc", gitHub = "eeefefefef"));
-    students.add(Student("A-B","Bbbbbb","Cccccc", email = "kk@gmial.com"));
-    students.add(Student("B","Vbbbbb","Cccccc", phoneNumber = "+79889889898"));
+    students.add(Student("Aabbb-Abbb","Bbbbbb","Cccccc", telegramValue = "@elelelelele"));
+    students.add(Student("Aaabbb","Bbb-Cbbb","Cccccc", gitHubValue = "eeefefefef"));
+    students.add(Student("A-B","Bbbbbb","Cccccc", emailValue = "kk@gmial.com"));
+    students.add(Student("B","Vbbbbb","Cccccc", phoneNumberValue = "+79889889898"));
     students.add(Student(hashMapOf(Pair("name","E"),Pair("surname","Bbbbbb"),Pair("patronymic","Cccccc"))));
     students.forEach { it: Student -> println(it) };
 
     println(Student(hashMapOf(Pair("name","E"),Pair("surname","Bbbbbb"),Pair("patronymic","Cccccc"))).validate())
-    println(Student("A-B","Bbbbbb","Cccccc", email = "kk@gmial.com", gitHub = "eeefefefef").validate());
+    println(Student("A-B","Bbbbbb","Cccccc", emailValue = "kk@gmial.com", gitHubValue = "eeefefefef").validate());
 
-    val stud = Student("A-B","Bbbbbb","Cccccc", email = "kk@gmial.com", gitHub = "eeefefefef", phoneNumber = "+79885556677");
-    stud.setContacts(hashMapOf(Pair("email","kk@gmial.com"),Pair("telegram","@elelelel"),Pair("gitHub",null)));
+    val stud = Student("A-B","Bbbbbb","Cccccc", emailValue = "kk@gmial.com", gitHubValue = "eeefefefef", phoneNumberValue = "+79885556677");
+    stud.setContacts(hashMapOf(Pair("email","kkkkk@gmial.com"),Pair("telegram","@elelelel"),Pair("gitHub",null)));
+    println(stud)
+    stud.name = "Abbbbcddd";
     println(stud)
 }
