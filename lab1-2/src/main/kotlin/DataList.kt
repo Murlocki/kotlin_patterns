d18dbee1d67e3007cec549352752007686c4896d
Murlocki <kirillkornilov03@gmail.com>
2024-09-15 18:08:05 +0300

    Lab 2.16-2.18 Рефакторинг кода

--- lab1-2/src/main/kotlin/DataList.kt
+++ lab1-2/src/main/kotlin/DataList.kt
@@ -1,4 +1,6 @@
-abstract class DataList<T>(val elements: Array<T>) {
+import kotlin.reflect.full.memberProperties
+
+open class DataList<T>(val elements: Array<T>) {
     private val selectedIndices = mutableSetOf<Int>()
     // Метод для выделения элемента по номеру
     fun select(number: Int) {
@@ -12,8 +14,22 @@ abstract class DataList<T>(val elements: Array<T>) {
     fun getSelectedIds() = selectedIndices.toIntArray()
 
     // Метод для получения массива наименований атрибутов (не реализован, так как не имеет информации об объектах)
-    abstract fun getNames(): Array<String>
+
+    open fun functionGetPropsNames() = if(this.elements.isEmpty()) arrayOf() else this.elements[0]!!::class.memberProperties.map{it.name}.toTypedArray<String>()
+    fun getNames(): Array<String> = functionGetPropsNames()
+
 
     // Метод для получения DataTable (не реализован, так как не имеет информации об объектах)
-    abstract fun getData(): DataTable
+    open fun getPropertiesOfClass(value:T):List<Any?> = arrayOf("12",2).toList()
+    fun getData(): DataTable {
+        val dataList:MutableList<Array<Any?>> = mutableListOf()
+        var rowNumber = 0;
+        for(el in this.elements){
+            dataList.add(
+                arrayOf(rowNumber,*getPropertiesOfClass(el).toTypedArray<Any?>())
+            )
+            rowNumber+=1;
+        }
+        return DataTable(dataList.toTypedArray<Array<Any?>>())
+    }
 }
--- lab1-2/src/test/kotlin/testStudent.kt
+++ lab1-2/src/test/kotlin/testStudent.kt
@@ -18,4 +18,17 @@ class testStudent {
         Student.writeToTxt("src/main/kotlin","out.txt",students);
         println(Student.readFromTxt("src/main/kotlin/out.txt").forEach { assert(it.getInfo()==students.find { el->el.id==it.id }?.getInfo()) })
     }
+    @Test
+    fun testDataListStudentShort(){
+        val students = mutableListOf<Student>();
+        students.add(Student("Aaaaa","Bbbbbb","Cccccc"));
+        students.add(Student("Aabbb-Abbb","Bbbbbb","Cccccc", telegramValue = "@elelelelele"));
+        students.add(Student("Aaabbb","Bbb-Cbbb","Cccccc", gitHubValue = "eeefefefef"));
+        students.add(Student("A-B","Bbbbbb","Cccccc", emailValue = "kk@gmial.com"));
+        students.add(Student("B","Vbbbbb","Cccccc", phoneNumberValue = "+79889889898"));
+        students.add(Student(hashMapOf(Pair("name","E"),Pair("surname","Bbbbbb"),Pair("patronymic","Cccccc"))));
+        var dat = DataListStudentShort(students.map { StudentShort(it) }.toTypedArray<StudentShort>())
+        assert(dat.getData().getRowCount()==6)
+        assert(dat.getData().getColumnCount()==4)
+    }
 }
