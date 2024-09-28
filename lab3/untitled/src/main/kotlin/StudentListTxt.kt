b237f9bdc439ebde3346f0e16c45e8cbba1de0ec
Murlocki <kirillkornilov03@gmail.com>
2024-09-28 21:22:06 +0300

    Lab 3.1. Добавлен круд студентов в списке

--- lab3/untitled/src/main/kotlin/StudentListTxt.kt
+++ lab3/untitled/src/main/kotlin/StudentListTxt.kt
@@ -3,6 +3,7 @@ package org.example
 import DataList
 import Student
 import StudentShort
+import copyIfPossible
 import java.io.File
 import java.io.FileWriter
 import kotlin.math.min
@@ -29,6 +30,32 @@ class StudentListTxt {
 
     fun getKNStudentShortList(k:Int,n:Int) = DataList<StudentShort>(studentList.slice(k*n..<min(k*n+n,studentList.size)).map { StudentShort(it) }.toTypedArray<StudentShort>());
 
-    fun sortyByInitials() = this.studentList.sortedBy { it.getInitials() }
+    fun sortByInitials() = this.studentList.sortedBy { it.getInitials() }
+
+
+    private fun addNewStudent(student:Student,id: Int = studentList.maxOf { it.id }+1){
+        val stud = Student(student.toString())
+        stud.id = id
+        this.studentList.add(stud)
+    }
+    fun addNewStudent(student:Student){
+        val stud = Student(student.toString())
+        stud.id = studentList.maxOf { it.id }+1
+        this.studentList.add(stud)
+    }
+    fun replaceById(id:Int,newStudent: Student){
+        val stud = Student(newStudent.toString())
+        stud.id = id
+        val ind = this.studentList.indexOf(this.studentList.find { it.id==id })
+        if(ind!=-1) this.studentList[ind] = stud
+        else this.addNewStudent(newStudent,id)
+    }
+
+    fun deletebyId(id: Int){
+        this.studentList.removeAt(this.studentList.indexOf(this.studentList.find { it.id==id }))
+    }
+    fun getStudentShortCount() = this.studentList.size
+
+    override fun toString() = this.studentList.toString()
 
 }
